3655eb9df86cbf62e873a41dcef6bba2ec1992e6
Luis18A <caballeroluis15@gmail.com>
2026-06-30 13:26:48 -0300

    Eliminar inicio de Celery de start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# Iniciar el Celery worker en segundo plano para procesar tareas asíncronas de Scraping
-echo "Iniciando Celery Worker..."
-python -m celery -A backend.tasks.celery worker --loglevel=info &
-
-# Iniciar la aplicación web principal de Flask con Gunicorn en primer plano
+# Iniciar la aplicación web principal de Flask con Gunicorn
 echo "Iniciando servidor web de Flask con Gunicorn..."
 python -m gunicorn app:app
